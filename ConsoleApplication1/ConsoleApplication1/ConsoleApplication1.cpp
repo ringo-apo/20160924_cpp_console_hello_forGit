d147b8e369339b056509e744642322bd02f14b47
akira taira <ringo1625@gmail.com>
2016-09-25 08:12:49 +0900

    最初のコミット

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+// ConsoleApplication1.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+	string a;
+	cout << "うれし恥ずかし、ハローワールド。ヽ(*'▽'*)ﾉ" << endl;
+	cout << endl;
+
+	cout << "何か文字を入力すると終了します。" << endl;
+	cin >> a;
+	return 0;
+}
+
